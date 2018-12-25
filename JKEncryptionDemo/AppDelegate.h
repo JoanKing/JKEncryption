cb7f33b848d8f886a41adae0dbaeedd4f689154d
JoanKingWC <JoanKingWC@163.com>
2018-12-25 20:50:08 +0800

    初始化项目

---
+++ JKEncryptionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKEncryptionDemo
+//
+//  Created by 王冲 on 2018/12/25.
+//  Copyright © 2018年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKEncryptionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKEncryptionDemo
+//
+//  Created by 王冲 on 2018/12/25.
+//  Copyright © 2018年 JK科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
